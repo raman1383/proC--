7c97db879e313d560fc8718af9c3dde5703e04c2
raman1383 <raman83shariati@gmail.com>
2022-07-10 17:19:31 +0430

    e

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-#include <numbers>
-
-int main()
+#include <stdio.h>
+int main(void)
 {
-
+	int answer, result;
+	answer = 100;
+	result = answer - 10;
+	printf("The result is %i\n", result + 5);
 	return 0;
 }
