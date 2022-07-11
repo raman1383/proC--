ab8d5a1034557d353b08bbaf947619d1c16ba517
raman1383 <raman83shariati@gmail.com>
2022-07-11 10:35:33 +0430

    r

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-
+#include "dijkstra.h"
 int main()
 {
-
+	int graph[6][6] = {
+		{0, 1, 2, 0, 0, 0},
+		{1, 0, 0, 5, 1, 0},
+		{2, 0, 0, 2, 3, 0},
+		{0, 5, 2, 0, 2, 2},
+		{0, 1, 3, 2, 0, 1},
+		{0, 0, 0, 2, 1, 0}};
+	dijkstra(graph, 0);
 	return 0;
 }
