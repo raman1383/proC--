89b8ed3e4b0a42d4f8bd9c93a10ba84afea7143d
raman1383 <raman83shariati@gmail.com>
2022-07-12 14:23:11 +0430

    t

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include "dijkstra.h"
 #include "bellman_ford.h"
 
-
-int main() {
+int main()
+{
     int graph[6][6] = {
-            {0, 1, 2, 0, 0, 0},
-            {1, 0, 0, 5, 1, 0},
-            {2, 0, 0, 2, 3, 0},
-            {0, 5, 2, 0, 2, 2},
-            {0, 1, 3, 2, 0, 1},
-            {0, 0, 0, 2, 1, 0}};
+        {0, 1, 2, 0, 0, 0},
+        {1, 0, 0, 5, 1, 0},
+        {2, 0, 0, 2, 3, 0},
+        {0, 5, 2, 0, 2, 2},
+        {0, 1, 3, 2, 0, 1},
+        {0, 0, 0, 2, 1, 0}};
     dijkstra(graph, 0);
-
 
     int V, E, gSrc;
     int src, dst, weight;
@@ -20,7 +19,8 @@ int main() {
     cout << "Enter number of edges: ";
     cin >> E;
     Graph G(V, E);
-    for (int i = 0; i < E; i++) {
+    for (int i = 0; i < E; i++)
+    {
         cout << "\nEdge " << i + 1 << "\nEnter source: ";
         cin >> src;
         cout << "Enter destination: ";
@@ -32,5 +32,4 @@ int main() {
     cout << "\nEnter source: ";
     cin >> gSrc;
     BellmanFord(G, gSrc);
-
 }
