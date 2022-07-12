f3ec9ccc976552e4aa4d757a1ab4693fb20da32b
raman1383 <raman83shariati@gmail.com>
2022-07-12 19:18:58 +0430

    r

--- funStuff/binarySearch.h
+++ funStuff/binarySearch.h
@@ -1,14 +1,14 @@
 #include <iostream> /// for IO operations
 #include <vector>
 
-uint64_t binarySearch(std::vector<uint64_t> arr, uint64_t val)
+int binarySearch(std::vector<int> arr, int val)
 {
-    uint64_t low = 0;               // set the lowest point of the vector.
-    uint64_t high = arr.size() - 1; // set the highest point of the vector.
+    int low = 0;               // set the lowest point of the vector.
+    int high = arr.size() - 1; // set the highest point of the vector.
 
     while (low <= high)
     {
-        uint64_t m = low + (high - low) / 2; // set the pivot point
+        int m = low + (high - low) / 2; // set the pivot point
 
         if (val == arr[m])
         {
